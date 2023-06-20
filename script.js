5e37980bcb972a4d9c17891a920a083184f4a68d
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-06-20 23:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
-    //Write your code here
-  
+   let radius_value= document.getElementById("radius").value;
+	let volume_val = (4/3) * Math.PI * Math.pow(radius_value, 3);
+	return (document.getElementById("volume").value = volume_val);
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
