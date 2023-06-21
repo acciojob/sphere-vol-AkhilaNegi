2adacde6fe811e7f3c893b86bfd286402747dac0
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-06-21 19:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function volume_sphere() {
-    //Write your code here
-var radius= document.getElementById("radius").value;
-var volume=(4/3)*Math.PI*  Math.pow(radius, 3);
-var volume=(4/3)*Math.PI* Math.pow(radius, 3);
-document.getElementById("volume").value = volume.toFixed(2);
-document.getElementById("submit").addEventListener("click", volume_sphere);
-// document.getElementById("submit").addEventListener("click", volume_sphere);
+function volume_sphere(e) {
+  //Write your code here
+	e.preventDefault();
+  let radius = document.getElementById("radius").value;
+  let vol = document.getElementById("volume");
+  // if (radius.length == 0) vol.value = "NaN";
+  for (let ele of radius) {
+    if (ele >= "0" && ele <= "9") {
+    } else {
+      vol.value = "NaN";
+      break;
+    }
+  }
 
-} 
+  if (vol.value !== "NaN")
+    vol.value = ((4 * Math.PI * radius * radius * radius) / 3).toFixed(4);
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
-document.getElementById('MyForm').addEventListener('submit', volume_sphere);
-document.getElementById('submit').addEventListener('click', volume_sphere);
+window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
